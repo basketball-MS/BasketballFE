c3872f55a89db3b86893a0cdce39607d114b0fac
dariusj13 <darius835@revature.net>
2022-08-25 12:41:51 -0500

    Added GM model

---
+++ src/app/models/GM.ts
@@ -0,0 +1,14 @@
+export class GM {
+
+    GM_id: number;
+    GM_Name: string;
+
+
+    constructor(GM_id: number, GM_Name: string) {
+        this.GM_id = GM_id;
+        this.GM_Name = GM_Name;
+    }
+
+
+
+}
