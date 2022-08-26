ee72ed5d8fc79235e2f27d225f5db1a1f59a7819
dariusj13 <darius835@revature.net>
2022-08-26 08:47:04 -0500

    Added base coach home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { TeamsPageComponent } from './components/teams-page/teams-page.component
 import { PlayerPageComponent } from './components/player-page/player-page.component';
 import { GMHomeComponent } from './components/gm-home/gm-home.component';
 import { LoginComponent } from './components/login/login.component';
+import { CoachHomeComponent } from './components/coach-home/coach-home.component';
 
 
 
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'teams', component: TeamsPageComponent },
   { path: 'players', component: PlayerPageComponent },
   { path: 'GM', component: GMHomeComponent },
+  { path: 'coach', component: CoachHomeComponent },
   //{ path: 'GM', redirectTo: "/GMHomeComponent", pathMatch:"full"},
   //{ path: 'player', redirectTo: "/PlayerHomeComponent", pathMatch:"full"},
   //{ path: 'coach', redirectTo: "/CoachHomeComponent", pathMatch:"full"}
