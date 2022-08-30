5b85d12cbaa962c065c4d47ed9915180c5ae21a8
apple <appleid@apples-iMac.local>
2022-08-30 16:43:54 -0400

    coach component

--- src/app/components/coach-home/coach-home.component.ts
+++ src/app/components/coach-home/coach-home.component.ts
@@ -12,4 +12,39 @@ export class CoachHomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+   
+  
+    selectedRowIds: Set<number> = new Set<number>();
+  
+    playersList: any[] = [
+      {playerId: 1, firstname:'Lebron', lastname: 'James', position: 'SF', time:'30'},
+      {playerId: 2, firstname:'Kyrie', lastname: 'Irving', position: 'PG', time:'30'}
+     
+    ];
+  
+    selectedId: string;
+  
+    onRowClick(id: number) {
+      if(this.selectedRowIds.has(id)) {
+       this.selectedRowIds.delete(id);
+      }
+      else {
+        this.selectedRowIds.add(id);
+      }
+    }
+  
+    rowIsSelected(id: number) {
+      return this.selectedRowIds.has(id);
+    }
+  
+    getSelectedRows(){
+      return this.playersList.filter(x => this.selectedRowIds.has(x.id));
+    }
+  
+    onLogClick() {
+      console.log(this.getSelectedRows());
+    }
+
+
+
 }
